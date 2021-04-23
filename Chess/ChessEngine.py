6bb1838869a6931ce360be7f63f3951d32af2d56
Mongosaurus <nathan.dygant@gmail.com>
2021-04-23 11:27:28 +0200

    Adding moving click logic and expanding the chess engine

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -22,3 +22,35 @@ class GameState:
         ]
         self.white_to_move = True
         self.move_log = []
+
+    def make_move(self, move):
+        self.board[move.start_row][move.start_column] = "--"
+        self.board[move.end_row][move.end_column] = move.piece_moved
+        self.move_log.append(move)  # Log the move so we can undo it later
+        self.white_to_move = not self.white_to_move
+
+class Move:
+    # Maps keys to values
+    # key : value
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                     "5": 3, "6": 2, "7": 1, "8": 0}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+    files_to_cols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                     "e": 4, "f": 5, "g": 6, "h": 7}
+    cols_to_files = {v: k for k, v in files_to_cols.items()}
+
+    def __init__(self, start_square, end_square, board):
+        self.start_row = start_square[0]
+        self.start_column = start_square[1]
+        self.end_row = end_square[0]
+        self.end_column = end_square[1]
+        self.piece_moved = board[self.start_row][self.start_column]
+        self.piece_captured = board[self.end_row][self.end_column]
+
+    def get_chess_notation(self):
+        # TODO: make this more "real" chess notation like
+        return self.get_rank_file(self.start_row, self.start_column) \
+               + self.get_rank_file(self.end_row, self.end_column)
+
+    def get_rank_file(self, row, col):
+        return self.cols_to_files[col] + self.rows_to_ranks[row]
